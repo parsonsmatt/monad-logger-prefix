6bb4bb3db07fccdeaba27c552376e07a9c44770d
Matt Parsons <parsonsmatt@gmail.com>
2016-07-12 12:06:21 -0400

    Add benchmark

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # [monad-logger-prefix][]
 
+[![Build Status](https://travis-ci.org/sellerlabs/monad-logger-prefix.svg?branch=master)](https://travis-ci.org/sellerlabs/monad-logger-prefix)
+
 This package provides an easy way to add prefixes to any
 [`MonadLogger`][monad-logger]. Here's a brief example:
 
@@ -19,6 +21,8 @@ main = runStdoutLoggingT $ do
         "bar" `prefixLogs` do
             $(logDebug) "This one has both [foo] and [bar] prefixes."
 ```
+
+The package includes a benchmark demonstrating that there is no performance difference with ordinary logging.
 
 [monad-logger-prefix]: https://github.com/githubuser/monad-logger-prefix
 [monad-logger]: https://hackage.haskell.org/package/monad-logger
--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Prelude hiding (log)
+
+import Control.Monad
 import Criterion.Main
 
 import Control.Monad.Logger
@@ -7,5 +12,28 @@ import Control.Monad.Logger.Prefix
 
 main :: IO ()
 main = defaultMain
-    [ bench "const" (whnf const ())
+    [ bgroup "no prefix"
+        [ bench "1: " $ whnfIO $ runStdoutLoggingT log
+        , bench "10: " $ whnfIO $ runStdoutLoggingT (10 `times` log)
+        , bench "100: " $ whnfIO $ runStdoutLoggingT (100 `times` log)
+        ]
+    , bgroup "one prefix"
+        [ bench "1: " $ whnfIO $ runStdoutLoggingT (pref log)
+        , bench "10: " $ whnfIO $ runStdoutLoggingT (10 `times` pref log)
+        , bench "100: " $ whnfIO $ runStdoutLoggingT (100 `times` pref log)
+        ]
+    , bgroup "two prefixes"
+        [ bench "1: " $ whnfIO $ runStdoutLoggingT (pref (pref log))
+        , bench "10: " $ whnfIO $ runStdoutLoggingT (10 `times` pref (pref log))
+        , bench "100: " $ whnfIO $ runStdoutLoggingT (100 `times` pref (pref log))
+        ]
     ]
+
+pref :: MonadLogger m => LogPrefixT m a -> m a
+pref = prefixLogs "foobar"
+
+log :: MonadLogger m => m ()
+log = logDebugN "hello world"
+
+times :: Monad m => Int -> m a -> m ()
+times i act = forM_ [1..i] (const act)
