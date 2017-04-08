63a21dc43c8d31fbfd6e2f0add4c6ed935368473
Matt Parsons <parsonsmatt@gmail.com>
2017-04-08 15:01:02 -0700

    this i a cleaner interface

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -2,34 +2,38 @@
 
 module Main where
 
-import Prelude hiding (log)
+import           Prelude                     hiding (log)
 
-import Control.Monad
-import Criterion.Main
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Criterion.Main
 
-import Control.Monad.Logger
-import Control.Monad.Logger.Prefix
+import           Control.Monad.Logger
+import           Control.Monad.Logger.Prefix
+
+logging :: MonadIO m => LogPrefixT (LoggingT m) a -> m a
+logging = runStdoutLoggingT . runLogPrefixT
 
 main :: IO ()
 main = defaultMain
     [ bgroup "no prefix"
-        [ bench "1: " $ whnfIO $ runStdoutLoggingT log
-        , bench "10: " $ whnfIO $ runStdoutLoggingT (10 `times` log)
-        , bench "100: " $ whnfIO $ runStdoutLoggingT (100 `times` log)
+        [ bench "1: " $ whnfIO $ logging log
+        , bench "10: " $ whnfIO $ logging (10 `times` log)
+        , bench "100: " $ whnfIO $ logging (100 `times` log)
         ]
     , bgroup "one prefix"
-        [ bench "1: " $ whnfIO $ runStdoutLoggingT (pref log)
-        , bench "10: " $ whnfIO $ runStdoutLoggingT (10 `times` pref log)
-        , bench "100: " $ whnfIO $ runStdoutLoggingT (100 `times` pref log)
+        [ bench "1: " $ whnfIO $ logging (pref log)
+        , bench "10: " $ whnfIO $ logging (10 `times` pref log)
+        , bench "100: " $ whnfIO $ logging (100 `times` pref log)
         ]
     , bgroup "two prefixes"
-        [ bench "1: " $ whnfIO $ runStdoutLoggingT (pref (pref log))
-        , bench "10: " $ whnfIO $ runStdoutLoggingT (10 `times` pref (pref log))
-        , bench "100: " $ whnfIO $ runStdoutLoggingT (100 `times` pref (pref log))
+        [ bench "1: " $ whnfIO $ logging (pref (pref log))
+        , bench "10: " $ whnfIO $ logging (10 `times` pref (pref log))
+        , bench "100: " $ whnfIO $ logging (100 `times` pref (pref log))
         ]
     ]
 
-pref :: MonadLogger m => LogPrefixT m a -> m a
+pref :: MonadLogger m => LogPrefixT m a -> LogPrefixT m a
 pref = prefixLogs "foobar"
 
 log :: MonadLogger m => m ()
